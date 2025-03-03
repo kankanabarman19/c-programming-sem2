5074d0eb1f2e2b2698fc1489b01625eb49b3d09d
kankanabarman19 <kankana1919@gmail.com>
2025-03-03 22:44:23 +0530

    Add files via upload

---
+++ Day 1/Substruction 2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	scanf ("%d",&a);
+	scanf ("%d",&b);
+	c=a-b;
+	printf ("The value of a is %d,The value of b is %d,Substruction of a&b is %d",
+	a,b,a-b);
+	return 0;
+}
